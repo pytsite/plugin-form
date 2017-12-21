66ef8524ea0f99f850d2742a9b4194dd9530b631
Alexander Shepetko <a@shepetko.com>
2017-12-21 12:48:21 +0200

    Minor update 1.2.2

--- __init__.py
+++ __init__.py
@@ -12,34 +12,35 @@ if _plugman.is_installed(__name__):
     from ._form import Form
 
 
-def plugin_load():
-    from pytsite import lang
+def _register_assetman_resources():
     from plugins import assetman
 
-    lang.register_package(__name__)
+    if not assetman.is_package_registered(__name__):
+        assetman.register_package(__name__)
+        assetman.t_less(__name__)
+        assetman.t_js(__name__)
+        assetman.js_module('pytsite-form-module', __name__ + '@js/pytsite-form-module')
 
-    assetman.register_package(__name__)
-    assetman.t_less(__name__)
-    assetman.t_js(__name__)
-    assetman.js_module('pytsite-form-module', __name__ + '@js/pytsite-form-module')
+    return assetman
+
+
+def plugin_install():
+    _register_assetman_resources().build(__name__)
+
+
+def plugin_load():
+    _register_assetman_resources()
 
 
 def plugin_load_uwsgi():
-    from pytsite import router, tpl
+    from pytsite import router, tpl, lang
     from plugins import http_api
     from . import _controllers, _http_api_controllers
 
+    lang.register_package(__name__)
     tpl.register_package(__name__)
 
     router.handle(_controllers.Submit, '/form/submit/<uid>', 'form@submit', methods='POST')
 
     http_api.handle('POST', 'form/widgets/<uid>', _http_api_controllers.GetWidgets, 'form@post_get_widgets')
     http_api.handle('POST', 'form/validate/<uid>', _http_api_controllers.PostValidate, 'form@post_validate')
-
-
-def plugin_install():
-    from plugins import assetman
-
-    plugin_load()
-    assetman.build(__name__)
-    assetman.build_translations()
